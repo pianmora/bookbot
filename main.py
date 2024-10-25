cd548a1c328873e6f87878aa016977d80a6a41eb
pianmora <martin.eichner@gmail.com>
2024-10-25 12:13:04 +0200

    added letter counting (lowercase)

--- main.py
+++ main.py
@@ -2,11 +2,23 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
+    num_letters = get_num_letters(text)
     print(f"{num_words} words found in the document")
+    print(f"{num_letters}")
 
 def get_num_words(text):
     words = text.split()
     return len(words)
+
+def get_num_letters(text):
+    letters = {}
+    for i in text:
+        l = i.lower()
+        if l in letters:
+            letters[l] += 1
+        else:
+            letters[l] = 1
+    return letters
 
 def get_book_text(path):
     with open(path) as f:
