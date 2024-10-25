3becbb55a5692b68c8c4778bab5b81ef45acd021
pianmora <martin.eichner@gmail.com>
2024-10-25 12:44:11 +0200

    output alphanumeric reverse sorted report (amount)

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ def main():
     num_words = get_num_words(text)
     num_letters = get_num_letters(text)
     print(f"{num_words} words found in the document")
-    print(f"{num_letters}")
+    print("----------------------------------------")
+    print_letter_report(num_letters)
 
 def get_num_words(text):
     words = text.split()
@@ -17,8 +18,22 @@ def get_num_letters(text):
         if l in letters:
             letters[l] += 1
         else:
-            letters[l] = 1
+            if l.isalpha():
+                letters[l] = 1
     return letters
+
+def sort_on(dict):
+    return dict["value"]
+
+def print_letter_report(letterlist):
+    letters = []
+    for i in letterlist:
+        letters.append({"key":i, "value": letterlist[i]})
+
+    letters.sort(reverse=True, key=sort_on)
+    for l in letters:
+        print(f"The \'{l['key']}\' character was found {l['value']} times")
+    return None
 
 def get_book_text(path):
     with open(path) as f:
